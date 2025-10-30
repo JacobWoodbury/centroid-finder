273737cc9fca0698901a45cb2055555f44fe02fc
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-10-30 11:56:24 -0700

    Server creation

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+//import db from "./models/index.js";
+import colors from "colors";
+import cors from "cors";
+
+dotenv.config();
+const { PORT } = process.env;
+
+const app = express();
+app.use(express());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//await db.sequelize.sync();
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`.bgYellow);
+});
